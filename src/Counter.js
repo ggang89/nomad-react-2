61882bd18d89344832d231cb4555d2fd81ac791d
ggang89 <ggang89@gmail.com>
2024-03-23 13:45:16 +0900

    useEffect관련 정리

--- src/Counter.js
+++ src/Counter.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
+//useEffect를 사용하면 렌더링 호출을 조정할 수 있다
 //useEffect : 2개의 argument를 가진다
-//첫번째 인자는 우리가 딱 한번만 실행하고 싶은 코드다
+//첫번째 인자는 우리가 실행하고 싶은 코드다
+//두번째 인자(dependency)가 변화할 때 react가 코드를 실행한다.
 function Counter() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
@@ -8,12 +10,12 @@ function Counter() {
   const onChange = (event) => setKeyword(event.target.value);
   console.log("All the time render");
   useEffect(() => {
-    //useEffect를 사용하면 렌더링 호출을 조정할 수 있다
     console.log("I run only once.");
   }, []);
   // 1번만 호출된다=> react가 지켜볼 게 없으니까
   useEffect(() => {
     if(keyword !=="" && keyword.length >5){
+      //""빈문자가 아니고, 키워드의 길이가 5개 이상일 때
       console.log("SEARCH FOR",keyword);
     }
   },[keyword]);
