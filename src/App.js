74419c2dd310975c9eb7ec7116f524b08cae135c
ggang89 <ggang89@gmail.com>
2024-03-23 14:14:49 +0900

    Cleanup 컴포넌트 생성 + showing useState로 text를 숨기고 보이는 것 연습

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import Button from "./Button";
 import styles from "./App.module.css";
 import Counter from "./Counter";
+import Cleanup from "./Cleanup";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
      <h1 className={styles.title}>Welcome back!</h1>
      <Button text={"Continue"}/>
      <Counter/>
+     <Cleanup/>
     </div>
   );
 }
---
+++ src/Cleanup.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Hello() {
+  return <h1>Hello</h1>;
+}
+//component는 jsx를 부르는 function이다
+function Cleanup() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
+  return (
+    <div>
+      {showing ? <Hello /> : null}
+      {/* 자바스크립트를 쓸 때 중괄호를 써준다 */}
+      <button onClick={onClick}>{showing ? "Hide" : "show"}</button>
+    </div>
+  );
+}
+export default Cleanup;
