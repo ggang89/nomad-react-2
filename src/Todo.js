cfad75338655be22c18b00ea20c32f1f06738c0b
ggang89 <ggang89@gmail.com>
2024-03-26 00:32:59 +0900

    여러개의 todo를 받을 수 있는 array 만듦

--- src/Todo.js
+++ src/Todo.js
@@ -1,25 +1,31 @@
-import {useState} from "react";
+import { useState } from "react";
 
-function Todo(){
-  const [toDo,setTodo] =useState("");
+function Todo() {
+  const [toDo, setTodo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setTodo(event.target.value);
-  const onSubmit =(event) =>{
+  const onSubmit = (event) => {
     event.preventDefault();
-
-  }
-return(
-<div>
-   <form onSubmit={onSubmit}>
-   <input
-    onChange={onChange} 
-    value={toDo}
-    type="text"
-     placeholder="Write your to do"/>
-     <button>Add To Do</button>
-   </form>
-  </div>
-)
-
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setTodo("");
+  };
+  return (
+    <div>
+      <h1>My To Dos({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do"
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default Todo;
