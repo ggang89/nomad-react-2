38f0df48e3fe5195152401c1ab6f09c018a7df2f
ggang89 <ggang89@gmail.com>
2024-03-28 18:01:48 +0900

    페이지배포 => npm i gh-pages =>npm run build =>패키지제이슨에서 홈페이지 주소 만들고, scripts에 deploy만들기

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path="/about-us">
-          <h1>Hello</h1>
-        </Route>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/about-us"/> 
+        <Route path="/movie/:id" element={ <Detail />}/>
+        <Route path="/" element={ <Home />}/>
+        {/* <Route path="*" element={<h1>Not Found Page</h1>}/> */}
+      </Routes>
     </Router>
   );
 }
