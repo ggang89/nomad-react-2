4070cdcec10e3307f8db9e9fc118aaddcb25d582
ggang89 <ggang89@gmail.com>
2024-03-28 18:08:59 +0900

    summary글자수를 235자 이하로 볼 수 있도록 수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-function Movie({ id,coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, summary, genres }) {
   //props의 이름은 자유롭게 정할 수 있다
   return (
     <div>
@@ -9,7 +9,7 @@ function Movie({ id,coverImg, title, summary, genres }) {
       <h2>
         <Link to={`/movie/${id}`}>{title}</Link>
       </h2>
-      <p>{summary}</p>
+      <p>{summary.length > 235 ? `${summary.slice(0,235)}...`:summary}</p>
       <ul>
         {genres.map((g) => (
           <li key={g}>{g}</li>
@@ -20,7 +20,7 @@ function Movie({ id,coverImg, title, summary, genres }) {
 }
 
 Movie.propTypes = {
-  id:PropTypes.number.isRequired,
+  id: PropTypes.number.isRequired,
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
